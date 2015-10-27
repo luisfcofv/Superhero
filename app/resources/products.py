613c1eb2cdb6b1f8592d887630ddebafed978230
Luis Flores <me@luisflores.mx>
2015-10-27 09:32:02 +0000

    Add sanity check

--- app/resources/products.py
+++ app/resources/products.py
@@ -6,6 +6,9 @@ class Products(Resource):
     def get(self, company_id):
         company = Company.query.get(company_id)
 
+        if company is None:
+            return 'Company not found', 400
+
         products_array = []
         for product in company.products:
             products_array.append(product.dictionary())
